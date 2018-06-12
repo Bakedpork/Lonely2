894feb94b7a4d7c900d2e449b39c0bf5d41ff4f6
Bakedpork <40089827+Bakedpork@users.noreply.github.com>
2018-06-12 16:39:35 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,7 +10,27 @@ client.on('message', message => {
     	message.reply('我最愛黑子的籃球了');
   	}
 });
+client.on('message', message => {
+    if (message.content === 'Lonely') {
+    	message.reply('公主.主人在叫我嗎?');
+  	}
+});
+client.on('message', message => {
+   if (message.content === 'Funny') {
+    	message.reply('我可不覺得.我很寂寞');
+  	}
+}); 
 
+client.on('message', message => {
+   if (message.content === 'XD') {
+    	message.reply('我可不覺得.我很寂寞');
+  	}
+}); 
+client.on('message', message => {
+   if (message.content === 'HAHA') {
+    	message.reply('我可不覺得.我很寂寞');
+  	}
+}); 
     
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
